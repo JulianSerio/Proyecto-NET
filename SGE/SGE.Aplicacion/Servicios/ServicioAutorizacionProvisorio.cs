c0319b95894a3cf877c85ec4a08ba8d56db86e96
SantiCiolfi <148510092+SantiCiolfi@users.noreply.github.com>
2024-05-04 00:07:32 -0300

    Add files via upload

--- SGE/SGE.Aplicacion/Servicios/ServicioAutorizacionProvisorio.cs
+++ SGE/SGE.Aplicacion/Servicios/ServicioAutorizacionProvisorio.cs
@@ -2,8 +2,17 @@
 
 public class ServicioAutorizacionProvisorio : IServicioAutorizacion
 {
-    public bool PoseeElPermiso(int IdUsuario, Permiso permiso) {
-        if (IdUsuario == 1) return true;
-        else return false;
+    public bool PoseeElPermiso(int idUsuario, Permiso permiso) {
+        try
+        {
+            if (idUsuario == 1) return true;
+            else throw new AutorizacionException();
+        }
+        catch (AutorizacionException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
     }
+
 }
