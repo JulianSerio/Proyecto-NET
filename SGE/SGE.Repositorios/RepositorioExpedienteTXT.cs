81fad75d173e26dbb35b9002c3d016dd7b2c27dc
SantiCiolfi <148510092+SantiCiolfi@users.noreply.github.com>
2024-05-04 00:08:42 -0300

    Add files via upload

--- SGE/SGE.Repositorios/RepositorioExpedienteTXT.cs
+++ SGE/SGE.Repositorios/RepositorioExpedienteTXT.cs
@@ -1,6 +1,23 @@
 ﻿namespace SGE.Repositorios;
+using SGE.Aplicacion;
 
 public class RepositorioExpedienteTXT
 {
-
+    readonly string _nameArch = "expedientes.txt";
+    public void ExpedienteAlta(Expediente expediente, int idUsuario)
+    {
+        try
+        {
+            if(ExpedienteValidador.validar(expediente.Caratula, idUsuario)){
+                using var sw = new StreamWriter(_nameArch, true);
+            }
+            else{
+                throw new ValidacionException();
+            }
+        }
+        catch (ValidacionException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
--- SGE/SGE.Repositorios/RepositorioTramiteTXT.cs
+++ SGE/SGE.Repositorios/RepositorioTramiteTXT.cs
@@ -1,6 +1,22 @@
 ﻿namespace SGE.Repositorios;
-
+using SGE.Aplicacion;
 public class RepositorioTramiteTXT
 {
-
+    readonly string _nameArch = "tramites.txt";
+    public void TramiteAlta(Tramite tramite, int idUsuario)
+    {
+        try
+        {
+            if(TramiteValidador.validar(tramite.Contenido, idUsuario)){
+                using var sw = new StreamWriter(_nameArch, true);
+            }
+            else{
+                throw new ValidacionException();
+            }
+        }
+        catch (ValidacionException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
