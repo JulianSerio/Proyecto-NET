4920e8bb6091e602bbf741e5b06629e02a4df92e
SantiCiolfi <148510092+SantiCiolfi@users.noreply.github.com>
2024-05-04 00:04:36 -0300

    Add files via upload

--- SGE/SGE.Aplicacion/Entidades/Expediente.cs
+++ SGE/SGE.Aplicacion/Entidades/Expediente.cs
@@ -8,7 +8,16 @@ public class Expediente{
     private int _idUsuarioModificador;
     private EstadoExpediente? _estado;
 
-
+    public Expediente(int id, string? caratula, DateTime creacion, DateTime modificacion, int idUsuarioModificador, EstadoExpediente? estado){
+        _id = id;
+        _caratula = caratula;
+        _fechaCreacion = creacion;
+        _fechaModificacion = modificacion;
+        _idUsuarioModificador = idUsuarioModificador;
+        _estado = estado;
+    }
     
-
+    public string? Caratula{
+        get => _caratula;
+    }
 }
--- SGE/SGE.Aplicacion/Entidades/Tramite.cs
+++ SGE/SGE.Aplicacion/Entidades/Tramite.cs
@@ -1,4 +1,6 @@
-﻿namespace SGE.Aplicacion;
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SGE.Aplicacion;
 
 public class Tramite {
     private int _id;
@@ -9,4 +11,16 @@ public class Tramite {
     private DateTime _fechaModificacion;
     private int _idUsuarioModificador;
 
+    public Tramite(int id, int expedienteID, string? contenido, DateTime creacion, DateTime modificacion, int idUsuarioModificador, EtiquetaTramite etiquetaTramite){
+        _id = id;
+        _contenido = contenido;
+        _fechaCreacion = creacion;
+        _fechaModificacion = modificacion;
+        _idUsuarioModificador = idUsuarioModificador;
+        _etiquetaTramite = etiquetaTramite;
+    }
+
+    public string? Contenido{
+        get => _contenido;
+    }
 }
