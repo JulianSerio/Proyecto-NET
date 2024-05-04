fe3dacb10c02179b90dead5d7a882c76860ed0a0
SantiCiolfi <148510092+SantiCiolfi@users.noreply.github.com>
2024-05-04 00:05:22 -0300

    Add files via upload

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteAlta.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteAlta.cs
@@ -1,6 +1,9 @@
 ﻿namespace SGE.Aplicacion;
 
-public class CasoDeUsoExpedienteAlta
+public class CasoDeUsoExpedienteAlta(IExpedienteRepositorio repo)
 {
-
+    public void Ejecutar(Expediente expediente, int idUsuario)
+    {
+        repo.ExpedienteAlta(expediente, idUsuario);
+    }
 }
