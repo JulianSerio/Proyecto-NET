ef6ca8ed75c448b3d87c2611245c6205eac40da8
Santiago Ciolfi <santiagociolfi@gmail.com>
2024-05-09 21:35:48 -0300

    Actualizacion del Program.cs

--- SGE/SGE.Consola/Program.cs
+++ SGE/SGE.Consola/Program.cs
@@ -13,45 +13,50 @@ var modificarExpediente = new CasoDeUsoExpedienteModificacion(autorizacion, repo
 // var actualizarEstado = new ServicioActualizacionEstado(repositorioExpedientes);
 
 //Test AltaExpediente
-/*
+
 altaExpediente.Ejecutar("Exp01", 1);
 altaExpediente.Ejecutar("", 1); //excepcion de validacion de expediente
 altaExpediente.Ejecutar("TestDe0", 0); //usuario no autorizado
 altaExpediente.Ejecutar("TestDeMayoresA0", 2); //usuario no autorizado
 altaExpediente.Ejecutar(null, 1); //excepcion de validacion de expediente
-*/
+
 
 //Test BajaExpediente
-/*
+
 bajaExpediente.Ejecutar(1, 1);//elimina al expediente con id=1
 bajaExpediente.Ejecutar(999, 1);//excepcion de repositorio, el id=999 no se encuentra en el repositorio
-*/
+
 
 //Test ConsultarExpedientesTodos
-/*
+
+altaExpediente.Ejecutar("Exp02", 1);
+altaExpediente.Ejecutar("Exp03", 1);
+altaExpediente.Ejecutar("Exp04", 1);
 List<Expediente> expedientes = consultarExpedienteTodos.Ejecutar();
 
 foreach (Expediente ex in expedientes){
-    Console.WriteLine(ex.Caratula);
+    Console.WriteLine($"Caratula:{ex.Caratula}, Estado:{ex.Estado}, Fecha de modificacion:{ex.FechaModificacion}");
 }
-*/
+
 
 // Test ModificarExpediente
-/*
-modificarExpediente.Ejecutar(1,1,"ExpedienteModificado01");
 
-expedientes = consultarExpedienteTodos.Ejecutar();
+modificarExpediente.Ejecutar(2,1,"ExpedienteModificado02");
 
-foreach (Expediente ex in expedientes){
-    Console.WriteLine(ex.Caratula);
+List<Expediente>expedientes2 = consultarExpedienteTodos.Ejecutar();
+
+foreach (Expediente ex in expedientes2){
+    Console.WriteLine($"Caratula:{ex.Caratula}, Estado:{ex.Estado}, Fecha de modificacion:{ex.FechaModificacion}");
 }
-*/
+
+modificarExpediente.Ejecutar(999, 1, "TestExpedienteInexistente"); //excepcion de repositorio, el id=999 no se encuentra en el repositorio
+
 
 //Test ConsultarExpedientePorId
-/*
-ConsultaExpediente? consul1 = consultarExpedientePorId.Ejecutar(1); //lista al expediente con id 1 con todos sus tramites
+
+ConsultaExpediente? consul1 = consultarExpedientePorId.Ejecutar(2); //lista al expediente con id 2 con todos sus tramites
 if(consul1.Expediente != null){
-    Console.WriteLine(consul1.Expediente.Caratula); //imprime la caratula del expediente
+    Console.WriteLine($"Caratula:{consul1.Expediente.Caratula}, Estado:{consul1.Expediente.Estado}, Fecha de modificacion:{consul1.Expediente.FechaModificacion}"); //imprime la caratula del expediente
     if(consul1.ListaTramites != null){
         foreach (Tramite tramite in consul1.ListaTramites)
         {
@@ -60,6 +65,7 @@ if(consul1.Expediente != null){
     }
 }
 ConsultaExpediente? consul2 = consultarExpedientePorId.Ejecutar(999); //excepcion de repositorio, el id=999 no se encuentra en el repositorio
+
 /*
 actualizarEstado.ModificarExpediente(3, etiqueta1); //respuesta esperada ConResolucion
 actualizarEstado.ModificarExpediente(1, etiqueta2); //El estado se mantiene igual
