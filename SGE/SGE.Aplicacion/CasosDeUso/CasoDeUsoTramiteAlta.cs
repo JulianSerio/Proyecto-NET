823e0c8a132b43c10d81971531e407d0d6ccec09
SantiCiolfi <148510092+SantiCiolfi@users.noreply.github.com>
2024-06-10 12:46:23 -0300

    Add files via upload

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteAlta.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteAlta.cs
@@ -2,15 +2,10 @@
 
 public class CasoDeUsoTramiteAlta(ITramiteRepositorio repo, ServicioAutorizacionProvisorio autorizacion, ServicioActualizacionEstado actualizacion) {
     public void Ejecutar(int expedienteID, EtiquetaTramite.Etiquetas etiqueta, string? contenido, int idUsuario){
-        try
-        {
-            if (autorizacion.PoseeElPermiso(idUsuario)) {//Verifico si el usuario tiene permisos
-                repo.TramiteAlta(expedienteID,etiqueta,contenido,DateTime.Now,idUsuario);
-                actualizacion.ModificarExpediente(expedienteID);  
-            } 
-        }
-        catch (ValidacionException){
-        }
+        if (autorizacion.PoseeElPermiso(idUsuario)) {//Verifico si el usuario tiene permisos
+            repo.TramiteAlta(expedienteID,etiqueta,contenido,DateTime.Now,idUsuario);
+            actualizacion.ModificarExpediente(expedienteID);  
+        } 
     }
     
 }
