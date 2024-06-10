914aaa21606eee2de0167dc6ae6845cd677cb966
SantiCiolfi <148510092+SantiCiolfi@users.noreply.github.com>
2024-06-10 12:45:14 -0300

    Add files via upload

--- SGE/SGE.Aplicacion/Validadores/ExpedienteValidador.cs
+++ SGE/SGE.Aplicacion/Validadores/ExpedienteValidador.cs
@@ -3,6 +3,6 @@
 public static class ExpedienteValidador
 {
     public static bool validar(string? caratula, int idUsuario){
-        return ((!string.IsNullOrEmpty(caratula) && !string.IsNullOrWhiteSpace(caratula))&&(idUsuario > 0)); //si la caratula no esta vacia, no es null y el id del usuario es mayor a 0 retorna verdadero
+        return ((!string.IsNullOrEmpty(caratula) && !string.IsNullOrWhiteSpace(caratula))&&(idUsuario > 0));
     }
 }
