6e64c57b4fb2dfed4de1f926e0d49862c14f2636
Lisandro Piva <lisandropiva@gmail.com>
2024-06-18 14:14:17 -0300

    termine tramites (faltarian un par de modificaciones y testear obvio)

--- SGE/SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
+++ SGE/SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a31a514799566e96f9641efc693adfd48c7c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82037402022e7ca14ce8d7b5300ff5c45878493")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
