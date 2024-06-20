9fc4eefa4136fcd5aa45f27807879b5844e205c1
Lisandro Piva <lisandropiva@gmail.com>
2024-06-20 18:35:25 -0300

    termine tramites

--- SGE/SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
+++ SGE/SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57acd413350cdd02547b378150da193728d54d85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189642f9c3a25b26cc26cf98660422d410a5d21a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
