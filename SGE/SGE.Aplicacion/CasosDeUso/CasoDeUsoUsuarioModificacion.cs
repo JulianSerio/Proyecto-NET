ead0d316c8bc0de253ea8545735da28ef007c0f2
Santiago Ciolfi <santiagociolfi@gmail.com>
2024-06-21 12:39:29 -0300

    Actualizacion final 21/6 12:39

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoUsuarioModificacion.cs
+++ SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoUsuarioModificacion.cs
@@ -3,7 +3,8 @@
 public class CasoDeUsoUsuarioModificacion(IUsuarioRepositorio repo, UsuarioValidador validador){
     public void Ejecutar(int IdUsuario,string nombre, string apellido, string email, string password, List<Permiso.Permisos> permisos){ //modificar permisos
         if(validador.Validar(email!)){ //el ! es para asegurarle al compilador de que email nunca puede ser null
-            repo.UsuarioModicacion(IdUsuario, nombre, apellido, email, password, permisos);
+            string passwordPasadaPorHash = ServicioFuncionHash.FuncionHashSHA256(password);
+            repo.UsuarioModicacion(IdUsuario, nombre, apellido, email, passwordPasadaPorHash, permisos);
         }else{
             throw new ValidacionException("El email ingresado ya se encuentra registrado en la base de datos");
         }
--- SGE/SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
+++ SGE/SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed948e6e1fc0ad461535651d9bd02a3a3408d01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15061eaea06deacd60c1bc040847a4d6ea1492b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
